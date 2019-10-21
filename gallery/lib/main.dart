96ac1388b95db51d36f2b018956f97ffc838eb45
rami-a <2364772+rami-a@users.noreply.github.com>
2019-10-21 16:10:23 -0400

    Enable desktop for macOS (#68)

--- gallery/lib/main.dart
+++ gallery/lib/main.dart
@@ -1,9 +1,27 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 import 'home.dart';
 import 'theme_data.dart';
 
-void main() => runApp(MyApp());
+void setOverrideForDesktop() {
+  if (kIsWeb) return;
+
+  if (Platform.isMacOS) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.iOS;
+  } else if (Platform.isLinux || Platform.isWindows) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  } else if (Platform.isFuchsia) {
+    debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  }
+}
+
+void main() {
+  setOverrideForDesktop();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
