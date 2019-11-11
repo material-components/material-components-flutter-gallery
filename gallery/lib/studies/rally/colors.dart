cd4ac1edca6c61550953afd1aee9c768f0299c2e
Rami Abou Ghanem <raboughanem@google.com>
2019-11-11 16:09:52 +0100

    Add copyright notice to top of each dart file

--- gallery/lib/studies/rally/colors.dart
+++ gallery/lib/studies/rally/colors.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'dart:ui';
 
 /// Most color assignments in Rally are not like the the typical color
--- gallery/lib/studies/rally/formatters.dart
+++ gallery/lib/studies/rally/formatters.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:intl/intl.dart';
 
 NumberFormat usdFormat = NumberFormat.currency(name: '');
