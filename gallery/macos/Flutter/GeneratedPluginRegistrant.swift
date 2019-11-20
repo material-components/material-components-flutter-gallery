6460407a31324849277892cacae9693045455ba3
rami-a <2364772+rami-a@users.noreply.github.com>
2019-11-20 15:16:33 +0100

    Make URL launcher work for macOS (#294)

--- gallery/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gallery/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import url_launcher_fde
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
