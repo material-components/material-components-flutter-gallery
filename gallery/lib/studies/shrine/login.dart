560c9f6e1434ae560a1d0b683dfa58455b26a992
Tianguang <penn.zht@gmail.com>
2019-11-25 18:04:05 +0100

    Shrine: Desktop login screen (#356)
    Add desktop login screen for shrine.

--- gallery/lib/studies/shrine/login.dart
+++ gallery/lib/studies/shrine/login.dart
@@ -14,8 +14,18 @@
 
 import 'package:flutter/material.dart';
 import 'package:gallery/data/gallery_options.dart';
+import 'package:gallery/layout/adaptive.dart';
 import 'package:gallery/l10n/gallery_localizations.dart';
 import 'package:gallery/studies/shrine/colors.dart';
+
+const desktopLoginScreenMainAreaWidth = 360.0;
+
+const _decoration = ShapeDecoration(
+  shape: BeveledRectangleBorder(
+    side: BorderSide(color: shrineBrown900, width: 0.5),
+    borderRadius: BorderRadius.all(Radius.circular(7)),
+  ),
+);
 
 class LoginPage extends StatefulWidget {
   @override
@@ -23,122 +33,193 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  static const ShapeDecoration _decoration = ShapeDecoration(
-    shape: BeveledRectangleBorder(
-      side: BorderSide(color: shrineBrown900, width: 0.5),
-      borderRadius: BorderRadius.all(Radius.circular(7)),
-    ),
-  );
+  @override
+  Widget build(BuildContext context) {
+    final bool isDesktop = isDisplayDesktop(context);
+
+    return ApplyTextOptions(
+      child: isDesktop
+          ? Scaffold(
+              body: SafeArea(
+                child: Center(
+                  child: Container(
+                    width: desktopLoginScreenMainAreaWidth,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        _ShrineLogo(),
+                        SizedBox(height: 40),
+                        _UsernameTextField(),
+                        SizedBox(height: 16),
+                        _PasswordTextField(),
+                        SizedBox(height: 24),
+                        _CancelAndNextButtons(),
+                        SizedBox(height: 62),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          : Scaffold(
+              appBar: AppBar(
+                elevation: 0,
+                backgroundColor: Colors.white,
+                brightness: Brightness.light,
+                leading: BackButton(
+                  onPressed: () {
+                    // The login screen is immediately displayed on top of the Shrine
+                    // home screen using onGenerateRoute and so rootNavigator must be
+                    // set to true in order to get out of Shrine completely.
+                    Navigator.of(context, rootNavigator: true).pop();
+                  },
+                ),
+              ),
+              body: SafeArea(
+                child: ListView(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  children: const [
+                    SizedBox(height: 80),
+                    _ShrineLogo(),
+                    SizedBox(height: 120),
+                    _UsernameTextField(),
+                    SizedBox(height: 12),
+                    _PasswordTextField(),
+                    _CancelAndNextButtons(),
+                  ],
+                ),
+              ),
+            ),
+    );
+  }
+}
+
+class _ShrineLogo extends StatelessWidget {
+  const _ShrineLogo();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image.asset('packages/shrine_images/diamond.png'),
+        const SizedBox(height: 16),
+        Text(
+          'SHRINE',
+          style: Theme.of(context).textTheme.headline,
+        ),
+      ],
+    );
+  }
+}
+
+class _UsernameTextField extends StatelessWidget {
+  const _UsernameTextField();
 
   @override
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
-    return ApplyTextOptions(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          brightness: Brightness.light,
-          leading: BackButton(
-            onPressed: () {
-              // The login screen is immediately displayed on top of the Shrine
-              // home screen using onGenerateRoute and so rootNavigator must be
-              // set to true in order to get out of Shrine completely.
-              Navigator.of(context, rootNavigator: true).pop();
-            },
-          ),
-        ),
-        body: SafeArea(
-          child: ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            children: [
-              const SizedBox(height: 80),
-              Column(
-                children: [
-                  Image.asset('packages/shrine_images/diamond.png'),
-                  const SizedBox(height: 16),
-                  Text(
-                    'SHRINE',
-                    style: Theme.of(context).textTheme.headline,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 120),
-              PrimaryColorOverride(
-                color: shrineBrown900,
-                child: Container(
-                  decoration: _decoration,
-                  child: TextField(
-                    controller: _usernameController,
-                    cursorColor: colorScheme.onSurface,
-                    decoration: InputDecoration(
-                      labelText: GalleryLocalizations.of(context)
-                          .shrineLoginUsernameLabel,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 12),
-              PrimaryColorOverride(
-                color: shrineBrown900,
-                child: Container(
-                  decoration: _decoration,
-                  child: TextField(
-                    controller: _passwordController,
-                    cursorColor: colorScheme.onSurface,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: GalleryLocalizations.of(context)
-                          .shrineLoginPasswordLabel,
-                    ),
-                  ),
-                ),
-              ),
-              Wrap(
-                children: [
-                  ButtonBar(
-                    children: [
-                      FlatButton(
-                        child: Text(
-                          GalleryLocalizations.of(context)
-                              .shrineCancelButtonCaption,
-                          style: TextStyle(color: colorScheme.onSurface),
-                        ),
-                        shape: const BeveledRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(7)),
-                        ),
-                        onPressed: () {
-                          // The login screen is immediately displayed on top of
-                          // the Shrine home screen using onGenerateRoute and so
-                          // rootNavigator must be set to true in order to get out
-                          // of Shrine completely.
-                          Navigator.of(context, rootNavigator: true).pop();
-                        },
-                      ),
-                      RaisedButton(
-                        child: Text(
-                          GalleryLocalizations.of(context)
-                              .shrineNextButtonCaption,
-                        ),
-                        elevation: 8,
-                        shape: const BeveledRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(7)),
-                        ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+    final TextEditingController _usernameController = TextEditingController();
+
+    return PrimaryColorOverride(
+      color: shrineBrown900,
+      child: Container(
+        decoration: _decoration,
+        child: TextField(
+          controller: _usernameController,
+          cursorColor: colorScheme.onSurface,
+          decoration: InputDecoration(
+            labelText:
+                GalleryLocalizations.of(context).shrineLoginUsernameLabel,
           ),
         ),
       ),
+    );
+  }
+}
+
+class _PasswordTextField extends StatelessWidget {
+  const _PasswordTextField();
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    final TextEditingController _passwordController = TextEditingController();
+
+    return PrimaryColorOverride(
+      color: shrineBrown900,
+      child: Container(
+        decoration: _decoration,
+        child: TextField(
+          controller: _passwordController,
+          cursorColor: colorScheme.onSurface,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText:
+                GalleryLocalizations.of(context).shrineLoginPasswordLabel,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CancelAndNextButtons extends StatelessWidget {
+  const _CancelAndNextButtons();
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    final bool isDesktop = isDisplayDesktop(context);
+
+    final EdgeInsets buttonTextPadding = isDesktop
+        ? EdgeInsets.symmetric(horizontal: 24, vertical: 16)
+        : EdgeInsets.zero;
+
+    return Wrap(
+      children: [
+        ButtonBar(
+          buttonPadding: isDesktop ? EdgeInsets.zero : null,
+          children: [
+            FlatButton(
+              child: Padding(
+                padding: buttonTextPadding,
+                child: Text(
+                  GalleryLocalizations.of(context).shrineCancelButtonCaption,
+                  style: TextStyle(color: colorScheme.onSurface),
+                ),
+              ),
+              shape: const BeveledRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(7)),
+              ),
+              onPressed: () {
+                // The login screen is immediately displayed on top of
+                // the Shrine home screen using onGenerateRoute and so
+                // rootNavigator must be set to true in order to get out
+                // of Shrine completely.
+                Navigator.of(context, rootNavigator: true).pop();
+              },
+            ),
+            RaisedButton(
+              child: Padding(
+                padding: buttonTextPadding,
+                child: Text(
+                  GalleryLocalizations.of(context).shrineNextButtonCaption,
+                ),
+              ),
+              elevation: 8,
+              shape: const BeveledRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(7)),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
